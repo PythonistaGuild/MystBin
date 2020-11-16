05174f64fdca9d0d47ae49e98b6561013e899c8b
Alex Nørgaard <Umbra@AbstractUmbra.dev>
2020-11-16 14:34:17 +0000

    models separation and cleaning

--- mystbin/rest/models/responses.py
+++ mystbin/rest/models/responses.py
@@ -16,75 +16,24 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with MystBin.  If not, see <https://www.gnu.org/licenses/>.
 """
-import datetime
+from datetime import datetime
 from typing import List, Optional
 
 from pydantic import BaseModel
-
-
-__all__ = (
-    "BadRequest",
-    "Forbidden",
-    "NotFound",
-    "PastePost",
-    "PastePatch",
-    "PasteDelete",
-    "PastePatchResponse",
-    "PastePostResponse",
-    "PasteGetResponse",
-    "PasteGetAllResponse",
-    "TokenResponse",
-    "Unauthorized",
-    "User",
-)
-
-
-class Unauthorized(BaseModel):
-    error: str = "Unauthorized"
-
-
-class Forbidden(BaseModel):
-    error: str = "Forbidden"
-
-
-class NotFound(BaseModel):
-    error: str = "Not Found"
-
-
-class BadRequest(BaseModel):
-    error: str = "Bad Request"
-    reason: str = None
-
-
-class PastePost(BaseModel):
-    content: str
-    syntax: Optional[str] = None
-    nick: Optional[str] = None
-    password: Optional[str] = None
-
-
-class PastePatch(BaseModel):
-    new_content: Optional[str] = None
-    new_nick: Optional[str] = None
-    new_expires: Optional[datetime.datetime] = None
-
-
-class PasteDelete(BaseModel):
-    pastes: List[str]
 
 
 class PastePostResponse(BaseModel):
     id: str
     nick: Optional[str] = None
     syntax: Optional[str] = None
-    created_at: datetime.datetime
-    expires: Optional[datetime.datetime] = None
+    created_at: datetime
+    expires: Optional[datetime] = None
 
 
 class PastePatchResponse(BaseModel):
     id: str
     nick: Optional[str] = None
-    expires: Optional[datetime.datetime] = None
+    expires: Optional[datetime] = None
 
 
 class PasteGetResponse(BaseModel):
@@ -92,8 +41,8 @@ class PasteGetResponse(BaseModel):
     content: str
     nick: Optional[str] = None
     syntax: Optional[str] = None
-    created_at: datetime.datetime
-    expires: Optional[datetime.datetime] = None
+    created_at: datetime
+    expires: Optional[datetime] = None
 
 
 class PasteGetAllResponse(BaseModel):
@@ -102,8 +51,8 @@ class PasteGetAllResponse(BaseModel):
     syntax: Optional[str] = None
     loc: int
     charcount: int
-    created_at: datetime.datetime
-    expires: Optional[datetime.datetime] = None
+    created_at: datetime
+    expires: Optional[datetime] = None
     has_password: bool
 
 
