29c65db5907a00959394db745fc4d84664f046f7
Eviee <evieepy@gmail.com>
2020-11-17 13:14:42 +1100

    Initial Front End design ideas.
    This code is messy and will change frequently.
    To run install yarn globally, change directory to 'mystbin/frontend' and run `yarn install` > `yarn dev`.

---
+++ mystbin/frontend/pages/[pid].tsx
@@ -0,0 +1,32 @@
+import { GetServerSideProps } from 'next'
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+import { PropsWithoutRef } from 'react'
+import styles from '../styles/Home.module.css'
+
+export default function Home(props: PropsWithoutRef<{authorized: boolean}>) {
+    const { authorized } = props;
+    const router = useRouter()
+    const { pid } = router.query
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      {authorized ? pid : "No."}
+    </div>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    let authorized = false;
+
+
+    return {
+        props: {
+            authorized,
+        },
+    }
+  }
