fd361044a541891ba5e248acad31f70f4308d59c
Eviee <evieepy@gmail.com>
2020-11-21 02:30:19 +1000

    Add actions buttons and opts divider.

--- mystbin/frontend/components/OptsBar.tsx
+++ mystbin/frontend/components/OptsBar.tsx
@@ -2,16 +2,42 @@ import {Nav, Navbar, OverlayTrigger, Popover} from "react-bootstrap";
 import React from "react";
 import EnhancedEncryptionIcon from '@material-ui/icons/EnhancedEncryption';
 import HourglassFullIcon from '@material-ui/icons/HourglassFull';
+import SaveAltIcon from '@material-ui/icons/SaveAlt';
+import EditIcon from '@material-ui/icons/Edit';
 import styles from "../styles/OptsBar.module.css"
 
 
 export default function OptsBar() {
-    const opts = [{title: "Create Password", content: "Create a password for this paste and all its files.", optional: true, icon: <EnhancedEncryptionIcon/>},
+    const actions = [{title: 'Save Paste', content: 'Save this paste and all its files.', icon: <SaveAltIcon />},
+                     {title: 'Edit Paste', content: 'Copy and Edit this paste as a new paste.', icon: <EditIcon />}]
+    const opts = [{title: "Create Password", content: "Create a password for this paste and all its files.", optional: true, icon: <EnhancedEncryptionIcon />},
                   {title: "Create Expiry", content: "Create a expiry date for this paste and all its files.", optional: false, icon: <HourglassFullIcon />}]
 
     return (
         <Navbar className="justify-content-center">
             <Nav className={styles.optsNavContainer}>
+                {actions.map((obj) => (
+                    <OverlayTrigger
+                        key={`opt-${obj.title}`}
+                        placement={"bottom"}
+                        overlay={
+                            <Popover>
+                                <Popover.Title
+                                    className={styles.popoverHeader}
+                                    as="h3">{obj.title}
+                                </Popover.Title>
+                                <Popover.Content>
+                                    {obj.content}
+                                </Popover.Content>
+                            </Popover>
+                        }
+                    >
+                        {obj.icon}
+                    </OverlayTrigger>
+                ))}
+
+                <div className={styles.optsDivider} />
+
                 {opts.map((obj) => (
                     <OverlayTrigger
                         key={`opt-${obj.title}`}
