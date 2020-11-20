1dbf8907adf61e1af9a2fa904d2d094d9d2f3748
Eviee <evieepy@gmail.com>
2020-11-20 23:54:26 +1000

    Basic options bar implementation.

--- mystbin/frontend/components/optsBar.tsx
+++ mystbin/frontend/components/optsBar.tsx
@@ -1,13 +1,36 @@
-import {Nav, Navbar} from "react-bootstrap";
+import {Nav, Navbar, OverlayTrigger, Popover} from "react-bootstrap";
 import React from "react";
 import EnhancedEncryptionIcon from '@material-ui/icons/EnhancedEncryption';
+import HourglassFullIcon from '@material-ui/icons/HourglassFull';
+import styles from "../styles/OptsBar.module.css"
 
 
 export default function OptsBar() {
+    const opts = [{title: "Create Password", content: "Create a password for this paste and all its files.", optional: true, icon: <EnhancedEncryptionIcon/>},
+                  {title: "Create Expiry", content: "Create a expiry date for this paste and all its files.", optional: false, icon: <HourglassFullIcon style={{marginLeft: "0.5rem"}} />}]
+
     return (
         <Navbar>
-            <Nav className="ml-auto">
-                <EnhancedEncryptionIcon></EnhancedEncryptionIcon>
+            <Nav className="mr-auto">
+                {opts.map((obj) => (
+                    <OverlayTrigger
+                        key={`opt-${obj.title}`}
+                        placement={"bottom"}
+                        overlay={
+                            <Popover>
+                                <Popover.Title
+                                    className={styles.popoverHeader}
+                                    as="h3">{obj.title}
+                                </Popover.Title>
+                                <Popover.Content>
+                                    {obj.content}<strong> {obj.optional? "Optional": "Required"}</strong>
+                                </Popover.Content>
+                            </Popover>
+                        }
+                    >
+                        {obj.icon}
+                    </OverlayTrigger>
+                    ))}
             </Nav>
         </Navbar>
     );
