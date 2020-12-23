ecdf5cb81cdceed97a88bc791222d67b190894ce
Alex Nørgaard <Umbra@AbstractUmbra.xyz>
2020-12-23 11:52:53 +0000

    Account for pyJWT 2.0.0 release and other deps updates

--- mystbin/rest/utils/tokens.py
+++ mystbin/rest/utils/tokens.py
@@ -4,18 +4,20 @@ from typing import Optional
 import jwt
 from Crypto.Random import get_random_bytes
 
+JWT_OPTIONS = {"verify_signature": False}
+
 
 def generate(userid: int, key=None) -> str:
     """Generate a token."""
     payload = {"id": userid}
     key = key or binascii.hexlify(get_random_bytes(64))
-    return jwt.encode(payload, key, algorithm='HS512').decode()
+    return jwt.encode(payload, key, algorithm="HS512").decode()
 
 
 def get_user_id(token: str) -> Optional[int]:
     """Get user ID from a token."""
     try:
-        payload = jwt.decode(token, verify=False, algorithms=['HS512'])
+        payload = jwt.decode(token, algorithms=['HS512'], options=JWT_OPTIONS)
         return payload['id']
     except jwt.DecodeError:
         return None
