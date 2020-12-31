88717b61d4ffa1916841ba1686ef75d9c1cf4624
NCPlayz <chowdhurynadir0@outlook.com>
2020-12-31 17:06:46 +0000

    move password input

--- mystbin/frontend/components/PasswordModal.tsx
+++ mystbin/frontend/components/PasswordModal.tsx
@@ -1,10 +1,9 @@
 import LockIcon from "@material-ui/icons/Lock";
-import { Button, Form, Modal } from "react-bootstrap";
-import VisibilityOffIcon from "@material-ui/icons/VisibilityOff";
-import VisibilityIcon from "@material-ui/icons/Visibility";
+import { Button, Modal } from "react-bootstrap";
 import Link from "next/link";
 import styles from "../styles/EditorTabs.module.css";
 import { useState } from "react";
+import PasswordInput from "./PasswordInput";
 
 export default function PasswordModal({
   show,
@@ -16,7 +15,6 @@ export default function PasswordModal({
   onAttempt: (arg0: string) => void;
 }) {
   const [passwordAttempt, setPasswordAttempt] = useState("");
-  const [passwordHide, setPasswordHide] = useState(true);
 
   return (
     <Modal
@@ -38,37 +36,11 @@ export default function PasswordModal({
       </Modal.Header>
       <Modal.Body>
         This paste is password protected. Please enter the password to continue.
-        <Form>
-          <Form.Group
-            controlId="pastePassword"
-            className={styles.passwordGroup}
-          >
-            <Form.Control
-              className={styles.passwordInput}
-              type={passwordHide ? "password" : "text"}
-              placeholder="Password"
-              onChange={(event) => {
-                setPasswordAttempt(event.currentTarget.value);
-              }}
-              onKeyDown={(e) => {
-                if (e.key === "Enter") {
-                  e.preventDefault();
-                  onAttempt(passwordAttempt);
-                }
-              }}
-            />
-            <span
-              className={
-                passwordHide
-                  ? styles.passwordVisibilityTrue
-                  : styles.passwordVisibilityFalse
-              }
-              onClick={(e) => setPasswordHide(!passwordHide)}
-            >
-              {passwordHide ? <VisibilityIcon /> : <VisibilityOffIcon />}
-            </span>
-          </Form.Group>
-        </Form>
+        <PasswordInput
+          value={passwordAttempt}
+          onChange={(value) => setPasswordAttempt(value)}
+          onSubmit={onAttempt}
+        />
       </Modal.Body>
       <Modal.Footer>
         <Link href={"/"}>Return to home</Link>
