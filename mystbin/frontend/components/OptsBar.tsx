f720eb904a90abcf6e92c6e9f1564d2b55df0674
PythonistaGuild Linter <lint-action@samuelmeuli.com>
2020-12-31 16:01:59 +0000

    Fix code style issues with Prettier

--- mystbin/frontend/components/OptsBar.tsx
+++ mystbin/frontend/components/OptsBar.tsx
@@ -1,5 +1,5 @@
-import {Modal, Nav, Navbar, OverlayTrigger, Popover} from "react-bootstrap";
-import React, {useState} from "react";
+import { Modal, Nav, Navbar, OverlayTrigger, Popover } from "react-bootstrap";
+import React, { useState } from "react";
 import EnhancedEncryptionIcon from "@material-ui/icons/EnhancedEncryption";
 import HourglassFullIcon from "@material-ui/icons/HourglassFull";
 import SaveAltIcon from "@material-ui/icons/SaveAlt";
@@ -38,70 +38,68 @@ export default function OptsBar() {
   ];
 
   return (
-      <div>
-        <Modal
-          show={showModal}
-          onHide={() => setShowModal(false)}
-          keyboard={false}
-          aria-labelledby="contained-modal-title-vcenter"
-          centered
-          className={styles.expiryModal}
-        >
-          <Modal.Header
-            className={styles.expiryModalHeader}>
-            <Modal.Title
-              className={styles.expiryModalTitle}>
-                Hello World
-            </Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            Set EXPIRY BRUH!
-          </Modal.Body>
-        </Modal>
+    <div>
+      <Modal
+        show={showModal}
+        onHide={() => setShowModal(false)}
+        keyboard={false}
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+        className={styles.expiryModal}
+      >
+        <Modal.Header className={styles.expiryModalHeader}>
+          <Modal.Title className={styles.expiryModalTitle}>
+            Hello World
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Set EXPIRY BRUH!</Modal.Body>
+      </Modal>
 
-    <Navbar className="justify-content-center">
-      <Nav className={styles.optsNavContainer}>
-        {actions.map((obj) => (
-          <OverlayTrigger
-            key={`opt-${obj.title}`}
-            placement={"bottom"}
-            overlay={
-              <Popover>
-                <Popover.Title className={styles.popoverHeader} as="h3">
-                  {obj.title}
-                </Popover.Title>
-                <Popover.Content>{obj.content}</Popover.Content>
-              </Popover>
-            }
-          >
-            <div className={styles.optsIconContainer}>{obj.icon}</div>
-          </OverlayTrigger>
-        ))}
+      <Navbar className="justify-content-center">
+        <Nav className={styles.optsNavContainer}>
+          {actions.map((obj) => (
+            <OverlayTrigger
+              key={`opt-${obj.title}`}
+              placement={"bottom"}
+              overlay={
+                <Popover>
+                  <Popover.Title className={styles.popoverHeader} as="h3">
+                    {obj.title}
+                  </Popover.Title>
+                  <Popover.Content>{obj.content}</Popover.Content>
+                </Popover>
+              }
+            >
+              <div className={styles.optsIconContainer}>{obj.icon}</div>
+            </OverlayTrigger>
+          ))}
 
-        {opts.map((obj) => (
-          <OverlayTrigger
-            key={`opt-${obj.title}`}
-            placement={"bottom"}
-            overlay={
-              <Popover>
-                <Popover.Title className={styles.popoverHeader} as="h3">
-                  {obj.title}
-                </Popover.Title>
-                <Popover.Content>
-                  {obj.content}
-                  <strong> {obj.optional ? "Optional" : "Required"}</strong>
-                </Popover.Content>
-              </Popover>
-            }
-          >
-            <div className={styles.optsIconContainer}
-              onClick={(e) => obj?.action(e)}>
-              {obj.icon}
-            </div>
-          </OverlayTrigger>
-        ))}
-      </Nav>
-    </Navbar>
-  </div>
+          {opts.map((obj) => (
+            <OverlayTrigger
+              key={`opt-${obj.title}`}
+              placement={"bottom"}
+              overlay={
+                <Popover>
+                  <Popover.Title className={styles.popoverHeader} as="h3">
+                    {obj.title}
+                  </Popover.Title>
+                  <Popover.Content>
+                    {obj.content}
+                    <strong> {obj.optional ? "Optional" : "Required"}</strong>
+                  </Popover.Content>
+                </Popover>
+              }
+            >
+              <div
+                className={styles.optsIconContainer}
+                onClick={(e) => obj?.action(e)}
+              >
+                {obj.icon}
+              </div>
+            </OverlayTrigger>
+          ))}
+        </Nav>
+      </Navbar>
+    </div>
   );
 }
