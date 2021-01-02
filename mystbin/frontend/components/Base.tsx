b1edac6781204fb4f27e736044c4749a706a7d61
Eviee <evieepy@gmail.com>
2021-01-02 20:39:40 +1000

    Complete page overhaul and redesign.

--- mystbin/frontend/components/Base.tsx
+++ mystbin/frontend/components/Base.tsx
@@ -1,17 +1,18 @@
 import { PropsWithChildren } from "react";
-import NavBar from "./NavBar";
+import OptsBar from "./OptsBar";
 import styles from "../styles/Base.module.css";
+import LogoMain from "../public/LogoMain";
 
 export default function Base(props: PropsWithChildren<{ className: string }>) {
   const { children, className } = props;
 
   return (
     <div className={styles.Base}>
-      <nav>
-        <NavBar />
-      </nav>
+      <OptsBar />
       <main className={className}>{children}</main>
-      <footer></footer>
+      <footer>
+        <LogoMain className={styles.logo} />
+      </footer>
     </div>
   );
 }
