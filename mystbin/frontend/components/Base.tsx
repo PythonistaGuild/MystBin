dc6b71e5d8edefdbaf91e97e0f0fe4198b759ccb
evieepy <evieepy@gmail.com>
2021-01-03 17:57:48 +1000

    Footer styling and additions.

--- mystbin/frontend/components/Base.tsx
+++ mystbin/frontend/components/Base.tsx
@@ -2,6 +2,9 @@ import { PropsWithChildren } from "react";
 import OptsBar from "./OptsBar";
 import styles from "../styles/Base.module.css";
 import LogoMain from "../public/LogoMain";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import DiscordColorIcon from "../icons/DiscordColour";
+import PatreonFireyIcon from "../icons/PatreonFirey";
 
 export default function Base(props: PropsWithChildren<{ className: string }>) {
   const { children, className } = props;
@@ -11,7 +14,13 @@ export default function Base(props: PropsWithChildren<{ className: string }>) {
       <OptsBar />
       <main className={className}>{children}</main>
       <footer>
+        <span className={styles.footerLeft}>Copyright © 2021 PythonistaGuild</span>
         <LogoMain className={styles.logo} />
+        <div className={styles.socialIconsContainer}>
+            <GitHubIcon className={styles.socialIcon} />
+            <DiscordColorIcon className={styles.socialIcon} />
+            <PatreonFireyIcon className={styles.socialIcon} />
+        </div>
       </footer>
     </div>
   );
