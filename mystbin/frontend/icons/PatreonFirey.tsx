4d9259647681fa94153ab637a4dcce052fe9ff67
PythonistaGuild Linter <lint-action@samuelmeuli.com>
2021-01-03 08:03:47 +0000

    Fix code style issues with Prettier

--- mystbin/frontend/icons/PatreonFirey.tsx
+++ mystbin/frontend/icons/PatreonFirey.tsx
@@ -1,19 +1,19 @@
-import * as React from "react"
+import * as React from "react";
 
 function PatreonFireyIcon(props: React.SVGProps<SVGSVGElement>) {
-    return (
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="1em"
-            height="1em"
-            viewBox="0 0 799.693 767.707"
-            {...props}
-        >
-            <g fill="#ff424d" fillRule="evenodd">
-                <path d="M512.652.001c-158.768 0-287.939 129.287-287.939 288.212 0 158.444 129.171 287.35 287.939 287.35 158.277 0 287.043-128.906 287.043-287.35C799.695 129.288 670.929.001 512.652.001M0 767.707h140.588V-.001H0v767.708" />
-            </g>
-        </svg>
-    )
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 799.693 767.707"
+      {...props}
+    >
+      <g fill="#ff424d" fillRule="evenodd">
+        <path d="M512.652.001c-158.768 0-287.939 129.287-287.939 288.212 0 158.444 129.171 287.35 287.939 287.35 158.277 0 287.043-128.906 287.043-287.35C799.695 129.288 670.929.001 512.652.001M0 767.707h140.588V-.001H0v767.708" />
+      </g>
+    </svg>
+  );
 }
 
-export default PatreonFireyIcon
+export default PatreonFireyIcon;
