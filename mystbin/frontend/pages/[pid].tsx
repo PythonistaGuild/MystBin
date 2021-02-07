d897448ff4783f3387bed37f0f36697991358bd5
evieepy <evieepy@gmail.com>
2021-02-07 20:19:02 +1000

    Fetch paste data and dynamic ID.

--- mystbin/frontend/pages/[pid].tsx
+++ mystbin/frontend/pages/[pid].tsx
@@ -1,31 +1,62 @@
-import { GetServerSideProps } from "next";
 import Head from "next/head";
-import { useRouter } from "next/router";
-import { PropsWithoutRef } from "react";
+import dynamic from "next/dynamic";
+import Base from "../components/Base";
 import styles from "../styles/Home.module.css";
+import { PropsWithoutRef } from "react";
+import { GetServerSideProps } from "next";
 
-export default function Home(props: PropsWithoutRef<{ authorized: boolean }>) {
-  const { authorized } = props;
-  const router = useRouter();
-  const { pid } = router.query;
+
+const PostMonacoEditor = dynamic(() => import("../components/EditorTabs"), {
+  ssr: false,
+});
+
+export default function Home(
+    props: PropsWithoutRef<{ paste }>
+) {
+  const { paste } = props;
+  let initialData = [];
+
+  if (paste['status'] === 401) {
+    initialData.push({'title': 'PASSWORD PROTECTED', 'content': 'This is a password protected paste.'})
+  }
+  else {
+    for (let file of paste['pastes']) {
+      initialData.push({'title': file['filename'], 'content': file['content']});
+    }
+  }
 
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      {authorized ? pid : "No."}
-    </div>
+      <div>
+        <Head>
+          <title>MystBin</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Base className={styles.Tabs}>
+          <PostMonacoEditor
+              initialData={initialData}
+              hasPassword={paste['status'] === 401}
+              pid={paste['pid']}
+          />
+        </Base>
+      </div>
   );
-}
+};
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  let authorized = false;
+export const getServerSideProps: GetServerSideProps = async ( { query} ) => {
+  const API = 'https://api-staging.mystb.in';
+
+  const { pid } = query;
+  const response = await fetch(`${API}/paste/${pid}`);
+
+  if (response.status === 404) {
+    console.log('Invalid Paste')
+  }
+
+  let paste = await response.json();
+  paste['status'] = response.status;
+  paste['pid'] = pid;
 
   return {
-    props: {
-      authorized,
-    },
+    props: { paste },
   };
 };
