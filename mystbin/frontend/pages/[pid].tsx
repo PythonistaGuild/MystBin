3e185285965f88d45c16ef43e05ea60550bed12a
Myst(MysterialPy) <mysterialpy@gmail.com>
2021-02-14 16:51:20 +1000

    Add meta tags.

--- mystbin/frontend/pages/[pid].tsx
+++ mystbin/frontend/pages/[pid].tsx
@@ -9,7 +9,7 @@ const PostMonacoEditor = dynamic(() => import("../components/EditorTabs"), {
   ssr: false,
 });
 
-export default function Home(props: PropsWithoutRef<{ paste }>) {
+export default function Paste(props: PropsWithoutRef<{ paste }>) {
   const { paste } = props;
   let initialData = [];
 
@@ -27,8 +27,12 @@ export default function Home(props: PropsWithoutRef<{ paste }>) {
   return (
     <div>
       <Head>
-        <title>MystBin</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title key="pasteTitle">MystBin - {paste['pid']}</title>
+        <meta property="og:title" content={`MystBin - ${paste["pid"]}`} key="pasteTitleOg"/>
+        <meta property="og:description" content={`This paste has (${paste["pastes"].length}) attached and expires in 2 hours.`} key="pasteDesc"/>
+        <meta property="og:image" content={"https://i.imgur.com/6aYhtEf.png"} key="pasteLogo"/>
+        <meta property="og:url" content={"https://staging.mystb.in/" + paste["pid"]} key="pasteUrl"/>
+
       </Head>
       <Base className={styles.Tabs}>
         <PostMonacoEditor
@@ -42,10 +46,12 @@ export default function Home(props: PropsWithoutRef<{ paste }>) {
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  console.log('SERVER')
   const API = "http://api:9000";
-
+  console.log(1)
   const { pid } = query;
   const response = await fetch(`${API}/paste/${pid}`);
+  console.log(1)
 
   if (response.status === 404) {
     console.log("Invalid Paste");
