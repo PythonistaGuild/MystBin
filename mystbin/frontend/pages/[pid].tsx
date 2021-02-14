c18269b9adc4f85bb8a456bd4e0ec6a53d787d8a
Myst(MysterialPy) <mysterialpy@gmail.com>
2021-02-15 09:01:16 +1000

    Add meta tags to [pid] routes.

--- mystbin/frontend/pages/[pid].tsx
+++ mystbin/frontend/pages/[pid].tsx
@@ -1,15 +1,16 @@
-import Head from "next/head";
 import dynamic from "next/dynamic";
 import Base from "../components/Base";
 import styles from "../styles/Home.module.css";
 import { PropsWithoutRef } from "react";
 import { GetServerSideProps } from "next";
+import Head from "next/head";
+
 
 const PostMonacoEditor = dynamic(() => import("../components/EditorTabs"), {
   ssr: false,
 });
 
-export default function Paste(props: PropsWithoutRef<{ paste }>) {
+export default function Pastey(props: PropsWithoutRef<{ paste }>) {
   const { paste } = props;
   let initialData = [];
 
@@ -27,27 +28,12 @@ export default function Paste(props: PropsWithoutRef<{ paste }>) {
   return (
     <div>
       <Head>
-        <title key="pasteTitle">MystBin - {paste["pid"]}</title>
-        <meta
-          property="og:title"
-          content={`MystBin - ${paste["pid"]}`}
-          key="pasteTitleOg"
-        />
-        <meta
-          property="og:description"
-          content={`This paste has (${paste["pastes"].length}) attached and expires in 2 hours.`}
-          key="pasteDesc"
-        />
-        <meta
-          property="og:image"
-          content={"https://i.imgur.com/6aYhtEf.png"}
-          key="pasteLogo"
-        />
-        <meta
-          property="og:url"
-          content={"https://staging.mystb.in/" + paste["pid"]}
-          key="pasteUrl"
-        />
+        <title key="pasteTitle">MystBin - {paste['pid']}</title>
+        <meta property="og:title" content={`MystBin - ${paste["pid"]}`} key="pasteTitleOg"/>
+        <meta property="og:description" content={`This paste has (${paste["pastes"].length}) attached file(s) and expires in 2 hours.`} key="pasteDesc"/>
+        <meta property="og:image" content={"https://i.imgur.com/6aYhtEf.png"} key="pasteLogo"/>
+        <meta property="og:url" content={"https://staging.mystb.in/" + paste["pid"]} key="pasteUrl"/>
+
       </Head>
       <Base className={styles.Tabs}>
         <PostMonacoEditor
@@ -61,12 +47,9 @@ export default function Paste(props: PropsWithoutRef<{ paste }>) {
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ query }) => {
-  console.log("SERVER");
   const API = "http://api:9000";
-  console.log(1);
   const { pid } = query;
   const response = await fetch(`${API}/paste/${pid}`);
-  console.log(1);
 
   if (response.status === 404) {
     console.log("Invalid Paste");
