4f27ef5afc2740994f85b8b1dfb12a87e6d18de4
Myst(MysterialPy) <mysterialpy@gmail.com>
2021-02-16 14:05:06 +1000

    Add google and github auth flows.

---
+++ mystbin/frontend/pages/github_auth.tsx
@@ -0,0 +1,25 @@
+import SleeperPush from "../components/Sleeper";
+import Cookies from "cookies";
+
+export default function GoogleAuth(props) {
+    const { token } = props;
+
+    return (
+        <div>
+            {token ? <SleeperPush ms={0} route={"/success"} /> : "ERROR"}</div>
+    );
+}
+
+export const getServerSideProps = async ({ req, res, query }) => {
+    let response = await fetch("http://api:9000/users/connect/github/", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(query),
+    });
+
+    const token = await response.json();
+    const cookies = new Cookies(req, res)
+    cookies.set('auth', token["token"], {httpOnly: true})
+
+    return { props: { token } };
+};
