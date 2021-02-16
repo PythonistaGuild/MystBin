a4786d36c4a6434201644d3baca5d6c1e5a343bd
Myst(MysterialPy) <mysterialpy@gmail.com>
2021-02-16 14:04:52 +1000

    Add dummy dashboard.

---
+++ mystbin/frontend/pages/dashboard.tsx
@@ -0,0 +1,18 @@
+import Cookies from "cookies";
+
+export default function DiscordAuth(props) {
+    const {status, msg} = props;
+    return (<div>{status}{msg}</div>)
+}
+
+export const getServerSideProps = async ({ req, res, query }) => {
+    const cookies = new Cookies(req, res);
+    const token = cookies.get('auth');
+
+    const resp = await fetch('http://api:9000/users/me', {
+        method: 'GET',
+        headers: {'Authorization': `Bearer ${token}`}
+    })
+
+    return {props: {status: resp.status, msg: resp.statusText}}
+}
