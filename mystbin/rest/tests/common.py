ddbc5fd94514c74275f38f96dca8158a1a7633c8
IAmTomahawkx <amanca@telus.net>
2021-02-17 16:33:18 -0800

    umbra is a monkey

---
+++ mystbin/rest/tests/common.py
@@ -0,0 +1,16 @@
+import sys
+import os
+import asyncio
+import pathlib
+import toml
+from httpx import AsyncClient
+from main import app
+
+with open("tests/config.toml") as f:
+    app.config = toml.load(f)
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(app.router.startup())
+
+
+client = AsyncClient(app=app, base_url="http://127.0.0.1:8888/")
