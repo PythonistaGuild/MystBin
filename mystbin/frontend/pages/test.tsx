791648972aab176dd77ee17bf1e368bf90abd75b
PythonistaGuild Linter <lint-action@samuelmeuli.com>
2021-04-20 05:07:54 +0000

    Fix code style issues with Prettier

--- mystbin/frontend/pages/test.tsx
+++ mystbin/frontend/pages/test.tsx
@@ -266,15 +266,17 @@ export default function Test(props) {
               className={styles.copyButton}
               onClick={() => {
                 fetch(config["site"]["backend_site"] + "/users/regenerate", {
-                  headers: { "Authorization": `Bearer ${token}` },
+                  headers: { Authorization: `Bearer ${token}` },
                   method: "POST",
                 }).then((result) => {
                   if (result.status === 200) {
                     result.json().then((data) => {
                       setToken(data["token"]);
-                      console.log(`Received new token ${data["token"]}/${result}/${data}`)
+                      console.log(
+                        `Received new token ${data["token"]}/${result}/${data}`
+                      );
                       cookieCutter.set("auth", data["token"]);
-                    })
+                    });
                   } else {
                     console.error(result.text());
                   }
@@ -376,7 +378,10 @@ export default function Test(props) {
               {!!google_id ? (
                 <BeenhereIcon className={styles.loginConfirmed} />
               ) : (
-                <AddBoxIcon className={styles.loginAddButton} href={`${config["site"][""]}`}/>
+                <AddBoxIcon
+                  className={styles.loginAddButton}
+                  href={`${config["site"][""]}`}
+                />
               )}
               {!!google_id ? "Account Linked" : "Link this account"}
             </div>
