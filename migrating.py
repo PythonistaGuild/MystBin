b212f3e843364d7b6c8d6d7dfa12f4c35b28a2b8
IAmTomahawkx <iamtomahawkx@gmail.com>
2022-08-09 15:49:53 -0700

    update migration script

--- migrating.py
+++ migrating.py
@@ -14,18 +14,22 @@ async def main():
         print(f"Exporting @ index {current_idx}")
         async with old.transaction():
             async with new.transaction():
-                bundle = await old.fetch("SELECT * FROM pastes ORDER BY id LIMIT 200")
+                bundle = await old.fetch("SELECT * FROM pastes ORDER BY id LIMIT 200 OFFSET $1", current_idx)
                 if not bundle:
                     print("Export complete")
                     break
+
+                print("fetched, inserting")
                     
-                await old.execute(f"DELETE FROM pastes WHERE {' OR '.join([f'id = ${x}' for x in range(1, 201)])}", *[x['id'] for x in bundle])
+                #await old.execute(f"DELETE FROM pastes WHERE {' OR '.join([f'id = ${x}' for x in range(1, 201)])}", *[x['id'] for x in bundle])
 
                 await new.executemany("INSERT INTO pastes (id, views, origin_ip) VALUES ($1, $2, '0.0.0.0')", [(x['paste_id'], x['views']) for x in bundle])
+                print("inserted pastes, inserting files")
                 await new.executemany(
                     "INSERT INTO files (parent_id, content, filename, loc) VALUES ($1, $2, 'migrated.txt', $3)",
                     [(x['id'], x['data'], x["data"].count("\n")) for x in bundle]
                 )
+                print("imported files")
                 current_idx += 200
 
 
