54c4dfdd4561c906f172a2ae32847a8bc30efb2c
IAmTomahawkx <iamtomahawkx@gmail.com>
2022-08-14 13:33:55 -0700

    fix typo

--- mystbin/backend/utils/ratelimits.py
+++ mystbin/backend/utils/ratelimits.py
@@ -217,7 +217,7 @@ class Limiter:
             }
             resp = await call_next(request)
             resp.headers.update(headers)
-            self.app.loop.create_task(self.app.state.db.put_logs(request, resp))
+            self.app.loop.create_task(self.app.state.db.put_log(request, resp))
             return resp
 
 
