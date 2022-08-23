21806dfb63d4163f712edbc96771f780f73ffb3e
IAmTomahawkx <iamtomahawkx@gmail.com>
2022-08-22 19:29:34 -0700

    change loc count to account for line 1

--- mystbin/backend/utils/db.py
+++ mystbin/backend/utils/db.py
@@ -269,7 +269,7 @@ class Database:
                         resp["id"],
                         page.content,
                         page.filename,
-                        page.content.count("\n"),
+                        page.content.count("\n") + 1, # add an extra for line 1
                     )
                 )
 
