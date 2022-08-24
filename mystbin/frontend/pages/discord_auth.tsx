bf31dd228e1506d9ffd40a9f4a7c5abf2cc1edfc
PythonistaGuild Linter <lint-action@samuelmeuli.com>
2022-08-24 15:58:25 +0000

    Fix code style issues with Prettier

--- mystbin/frontend/pages/discord_auth.tsx
+++ mystbin/frontend/pages/discord_auth.tsx
@@ -11,7 +11,7 @@ export default function DiscordAuth(props) {
 }
 
 export const getServerSideProps = async ({ req, res, query }) => {
-  console.error(1)
+  console.error(1);
   const cookies = new Cookies(req, res);
   let headers;
 
@@ -36,9 +36,9 @@ export const getServerSideProps = async ({ req, res, query }) => {
     }
   );
 
-  console.error(2)
+  console.error(2);
   const token = await response.json();
   cookies.set("auth", token["token"], { httpOnly: false });
-  console.error(3)
+  console.error(3);
   return { props: { token } };
 };
