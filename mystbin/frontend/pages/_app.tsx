42a986b8c0d29e1fb16d615530cfa0a2598d2867
Myst(MysterialPy) <mysterialpy@gmail.com>
2022-08-28 22:18:36 +1000

    Update app page.

--- mystbin/frontend/pages/_app.tsx
+++ mystbin/frontend/pages/_app.tsx
@@ -1,8 +1,13 @@
 import "../styles/globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Script from "next/Script"
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return(<>
+  <Script src={"https://media.ethicalads.io/media/client/ethicalads.min.js"} strategy={"beforeInteractive"}/>
+  <Component {...pageProps} />;
+    </>)
 }
 
 export default MyApp;
