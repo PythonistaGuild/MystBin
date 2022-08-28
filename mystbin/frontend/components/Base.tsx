c11a21edc20841a7d645bdbc44effac9c542b4d3
PythonistaGuild Linter <lint-action@samuelmeuli.com>
2022-08-28 10:48:26 +0000

    Fix code style issues with Prettier

--- mystbin/frontend/components/Base.tsx
+++ mystbin/frontend/components/Base.tsx
@@ -11,7 +11,11 @@ export default function Base(props: PropsWithChildren<{ className: string }>) {
 
   return (
     <div className={styles.Base}>
-    <div data-ea-publisher="mystbin" data-ea-type="text" data-ea-style="fixedfooter" ></div>
+      <div
+        data-ea-publisher="mystbin"
+        data-ea-type="text"
+        data-ea-style="fixedfooter"
+      ></div>
 
       <OptsBar />
       <TipModal />
