9742dadf3ec7d1c6e7c6a8ac188b71a0a41adebe
Myst(MysterialPy) <mysterialpy@gmail.com>
2022-08-29 10:17:25 +1000

    Add ethical ads to body

---
+++ mystbin/frontend/pages/_document.jsx
@@ -0,0 +1,31 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+
+  return (
+
+    <Html>
+
+      <Head />
+
+      <body>
+<div
+
+        data-ea-publisher="mystbin"
+
+        data-ea-type="text"
+
+        data-ea-style="fixedfooter"
+
+      ></div>
+        <Main />
+
+        <NextScript />
+
+      </body>
+
+    </Html>
+
+  )
+
+}
