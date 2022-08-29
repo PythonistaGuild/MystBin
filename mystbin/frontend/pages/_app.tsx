d824ac8080e690c91fb07b5ab05533d6fd72d193
Myst(MysterialPy) <mysterialpy@gmail.com>
2022-08-29 13:28:38 +1000

    Ugh broken script again

--- mystbin/frontend/pages/_app.tsx
+++ mystbin/frontend/pages/_app.tsx
@@ -5,7 +5,7 @@ import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   return(<>
-  <Script async="async" src={"https://media.ethicalads.io/media/client/ethicalads.min.js"} strategy={"beforeInteractive"}/>
+  <Script async={"async"} src={"https://media.ethicalads.io/media/client/ethicalads.min.js"} strategy={"beforeInteractive"}/>
   <Component {...pageProps} />;
     </>)
 }
