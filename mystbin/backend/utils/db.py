4ea1e14b20a24ff7cc953397445d60f31785193e
Alex Nørgaard <Umbra@AbstractUmbra.dev>
2022-08-29 20:15:25 +0100

    fix more annotations

--- mystbin/backend/utils/db.py
+++ mystbin/backend/utils/db.py
@@ -16,6 +16,8 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with MystBin.  If not, see <https://www.gnu.org/licenses/>.
 """
+from __future__ import annotations
+
 import asyncio
 import datetime
 import difflib
