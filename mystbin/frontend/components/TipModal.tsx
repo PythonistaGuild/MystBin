4e7d029b369c892b6a92fcc7325d8915cafbda9e
EvieePy <evieepy@gmail.com>
2022-10-04 21:15:31 +1000

    Added tips to Tip Modal.

--- mystbin/frontend/components/TipModal.tsx
+++ mystbin/frontend/components/TipModal.tsx
@@ -5,6 +5,7 @@ import cookieCutter from "cookie-cutter";
 
 export default function TipModal() {
   const [showTip, setShowTip] = useState(true);
+  const [tab, setTab] = useState(0);
 
   useEffect(() => {
     const showCookie = cookieCutter.get("showTips");
@@ -16,8 +17,27 @@ export default function TipModal() {
 
   function handleClick() {
     setShowTip(false);
-    cookieCutter.set("showTips", false);
+
+    const date = new Date();
+    date.setDate(date.getDate() + 14);
+    cookieCutter.set("showTips", false, { expires: date });
   }
+
+  function handleChange() {
+    if (tab + 1 === tabs.length) {
+      setTab(0);
+    }
+
+    else {
+      setTab(tab + 1)
+    }
+  }
+
+  const tabs = [
+      'You can change the filename and extension? Changing the extension lets you change the language your paste will be highlighted in. For example, for Python scripts, change the extension to .py',
+      'You can now Drag and Drop named files into the editor to copy it\'s contents, name and file extension.',
+      'You can now add images to your paste files. Click on + symbol on your file tab to add an image.',
+  ]
 
   return (
     <>
@@ -27,14 +47,14 @@ export default function TipModal() {
           <br />
           <br />
           <span style={{ fontSize: "0.9em" }}>
-            <b>Did you know:</b> You can change the filename and extension?
-            Changing the extension lets you change the language your paste will
-            be highlighted in. For example, for Python scripts, change the
-            extension to .py
+            <b>Did you know:</b><br/>{tabs[tab]}
           </span>
           <br />
           <br />
-          <Button onClick={handleClick}>Ok</Button>
+          <div className={styles.bottomButtons}>
+            <Button className={styles.closeButton} onClick={handleClick}>Ok</Button>
+            <Button className={styles.nextButton} onClick={handleChange}>Next</Button>
+          </div>
         </div>
       ) : null}
     </>
