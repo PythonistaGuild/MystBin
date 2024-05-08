20881dc8ba503c697bc32bb2a0f670b4c608a0b0
MystyPy <evieepy@gmail.com>
2024-05-09 02:41:30 +1000

    Added automatic files; removed add file button

--- web/static/scripts/files.js
+++ web/static/scripts/files.js
@@ -1,51 +1,68 @@
 let pasteContainer = document.querySelector(".pasteContainer");
-let addButton = document.querySelector(".addPaste");
-let count = 0;
+let count = 1;
 
-addButton.addEventListener("click", (e) => {
+
+function addFile(number) {
     let files = pasteContainer.getElementsByClassName("pasteArea");
+    let currentArea = document.getElementById(`__file${number}`);
+
+    if (currentArea.querySelector("[name='fileContent']").value) {
+        currentArea.classList.remove("smallArea");
+    } else if (currentArea !== files[0]) {
+        currentArea.classList.add("smallArea");
+    }
 
     if (files.length >= 5) {
         return;
+    }
+
+    for (let file of files) {
+        let textArea = file.querySelector("[name='fileContent']");
+        if (!textArea || !textArea.value) { return }
     }
 
     count += 1;
 
-    const pasteHTML = `<div class="pasteArea" id="__file${count}">
-<div class="pasteHeader">
-  <textarea name="fileName" class="filenameArea" rows="1" placeholder="Optional Filename..." maxlength="25"></textarea>
-  <span class="deleteFile" onclick="deleteFile('__file${count}')">Delete File</span>
-</div>
-<textarea name="fileContent" required autofocus spellcheck="false" placeholder="Paste code or text..." maxlength="300000"></textarea>
-</div>`;
+    const pasteHTML = `
+    <div class="pasteArea smallArea" id="__file${count}" data-position="${count}">
+        <div class="pasteHeader">
+            <textarea name="fileName" class="filenameArea" rows="1" placeholder="Optional Filename..." maxlength="25"></textarea>
+            <span class="deleteFile" onclick="deleteFile('__file${count}')">Delete File</span>
+        </div>
+        <textarea name="fileContent" required autofocus spellcheck="false" placeholder="Paste code or text..." maxlength="300000" onkeyup="addFile(${count})"></textarea>
+    </div>`;
 
     pasteContainer.insertAdjacentHTML("beforeend", pasteHTML);
-
     files = pasteContainer.getElementsByClassName("pasteArea");
-    for (let file of files) {
-        file.querySelector(".pasteHeader .deleteFile").classList.remove("disabled");
+    if (files.length >= 5) { return }
+    else if (files.length <= 2) {
+        for (let file of files) {
+            file.querySelector(".pasteHeader .deleteFile").classList.add("disabled");
+        }
     }
-
-    if (files.length >= 5) {
-        addButton.style.display = "none";
+    else {
+        for (let file of files) {
+            file.querySelector(".pasteHeader .deleteFile").classList.remove("disabled");
+        }
     }
-});
+}
 
 function deleteFile(identifier) {
     let files = pasteContainer.getElementsByClassName("pasteArea");
 
-    if (files.length == 1) {
+    if (files.length <= 2) {
         return;
-    } else {
-        addButton.style.display = "flex";
     }
 
-    document.getElementById(identifier).remove();
+    let file = document.getElementById(identifier);
+    file.remove();
 
     files = pasteContainer.getElementsByClassName("pasteArea");
-    if (files.length == 1) {
-        files[0]
-            .querySelector(".pasteHeader .deleteFile")
-            .classList.add("disabled");
+    if (files.length <= 2) {
+        files[0].classList.remove("smallArea");
+
+        for (let file of files) {
+            file.querySelector(".pasteHeader .deleteFile").classList.add("disabled");
+        }
     }
 }
