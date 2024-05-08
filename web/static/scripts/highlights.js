68a44739bfc2b0649e4fad689f75e024d6bee394
MystyPy <evieepy@gmail.com>
2024-05-08 22:19:15 +1000

    Sadly this needs to be defered.

--- web/static/scripts/highlights.js
+++ web/static/scripts/highlights.js
@@ -1,11 +1,10 @@
 let pasteStores = [];
 
+
+const codes = document.querySelectorAll("pre > code");
+for (let code of codes) {
+    pasteStores.push(code.textContent);
+}
+
 hljs.highlightAll();
 hljs.initLineNumbersOnLoad();
-
-window.onload = (e) => {
-    const codes = document.querySelectorAll("pre > code");
-    for (let code of codes) {
-        pasteStores.push(code.textContent);
-    }
-}
