5a9a16bff674459462d574f7c96e7c6dadd1d746
MystyPy <evieepy@gmail.com>
2024-05-08 21:57:20 +1000

    Possible speedups; Removed a fetch call and unnecessary CSS/JS loading.

--- web/static/scripts/highlights.js
+++ web/static/scripts/highlights.js
@@ -1,18 +1,11 @@
 let pasteStores = [];
 
+hljs.highlightAll();
+hljs.initLineNumbersOnLoad();
 
-document.addEventListener("htmx:afterRequest", function (evt) {
-    if (evt.detail.xhr.status != 200) {
-        return
+window.onload = (e) => {
+    const codes = document.querySelectorAll("pre > code");
+    for (let code of codes) {
+        pasteStores.push(code.textContent);
     }
-
-    if (evt.detail.target.id == "pastecontainer" || evt.detail.target.id == "content") {
-        const codes = document.querySelectorAll("pre > code");
-        for (let code of codes) {
-            pasteStores.push(code.textContent);
-        }
-
-        hljs.highlightAll();
-        hljs.initLineNumbersOnLoad();
-    }
-});
+}
